de3a13e92391551a92685722d978b6d91919edcb
AryaCodes <55866147+AryaCodes@users.noreply.github.com>
2020-10-08 05:15:44 +0530

    Add files via upload

--- addingmachine.java
+++ addingmachine.java
@@ -1,33 +1,45 @@
+//Name: Arya Tawde
+//ASU ID: 1217536502
+//Assignment02 CSE360
 package cse360assignment02;
 
 //This is a simple class which adds and subtracts.
 public class AddingMachine {
   private int total;
+  private String transactions;
   
 //Public constructor of the class.
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
+    transactions = "0";
   }
   
 //Public method, returns stored total value
   public int getTotal () {
-    return 0;
+    return this.total;
   }
 
 //Adds value to total  
   public void add (int value) {
+	this.total = this.total + value;
+	transactions = transactions + "+" + value;
   }
 
 //Subtracts value from total
-  public void subtract (int value) {
-  }
+  public void subtract (int value) { 
+	this.total = this.total - value;
+	transactions = transactions + "-" + value; 
+ }
+
 
 //Overriding toString of main.
   public String toString () {
-    return "";
+    return transactions ;
   }
 
 //Clears the currently stored total in the class.
   public void clear() {
+    total = 0;  
+    transactions = "0";
   }
 }
