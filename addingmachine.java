f82a314bb86bd654b8ded84696c243c8e1e1ed1b
AryaCodes <55866147+AryaCodes@users.noreply.github.com>
2020-10-08 04:58:24 +0530

    skeletal structure

---
+++ addingmachine.java
@@ -0,0 +1,33 @@
+package cse360assignment02;
+
+//This is a simple class which adds and subtracts.
+public class AddingMachine {
+  private int total;
+  
+//Public constructor of the class.
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+//Public method, returns stored total value
+  public int getTotal () {
+    return 0;
+  }
+
+//Adds value to total  
+  public void add (int value) {
+  }
+
+//Subtracts value from total
+  public void subtract (int value) {
+  }
+
+//Overriding toString of main.
+  public String toString () {
+    return "";
+  }
+
+//Clears the currently stored total in the class.
+  public void clear() {
+  }
+}
